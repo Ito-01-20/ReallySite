00110daf1d07bf8a1b1e25cd276cee2f3ed01829
Ito-01-20 <kaiki_2001_hebi@icloud.com>
2021-11-29 13:40:28 +0900

    blog2

--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Article
 
 # Create your views here.
 def index(request):
+    objs = Article.objects.all()[:3]
     context = {
-        'title': 'Really Site'
+        'title': 'Really Site',
+        'articles': objs,
     }
     return render(request, 'mysite/index.html', context)
